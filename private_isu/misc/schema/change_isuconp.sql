931c9f5f3f2b0c60e7cccd66f896c119d91a7378
kensei <kensei.kitahara@gmail.com>
2016-09-09 14:05:45 +0900

    add posts index : score 9449

--- private_isu/misc/schema/change_isuconp.sql
+++ private_isu/misc/schema/change_isuconp.sql
@@ -1,2 +1,3 @@
 CREATE INDEX post_id_idx ON comments (post_id);
 CREATE INDEX post_id_created_at_idx ON comments (post_id, created_at);
+CREATE INDEX user_id_created_at_idx ON posts (user_id, created_at);
