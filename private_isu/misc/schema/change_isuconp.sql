55d2aaf1dab540f586184d274a974888015ba5a5
kensei <kensei.kitahara@gmail.com>
2016-09-09 14:15:06 +0900

    add index on commnents : score 9811

--- private_isu/misc/schema/change_isuconp.sql
+++ private_isu/misc/schema/change_isuconp.sql
@@ -1,3 +1,3 @@
-CREATE INDEX post_id_idx ON comments (post_id);
 CREATE INDEX post_id_created_at_idx ON comments (post_id, created_at);
+CREATE INDEX user_id_idx ON comments (user_id);
 CREATE INDEX user_id_created_at_idx ON posts (user_id, created_at);
