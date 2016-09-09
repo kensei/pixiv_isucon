ac7e2e74729720a7eaa141fb491d1b41267cbee7
kensei <kensei.kitahara@gmail.com>
2016-09-09 14:17:43 +0900

    add index on posts : score 9894

--- private_isu/misc/schema/change_isuconp.sql
+++ private_isu/misc/schema/change_isuconp.sql
@@ -1,3 +1,4 @@
 CREATE INDEX post_id_created_at_idx ON comments (post_id, created_at);
 CREATE INDEX user_id_idx ON comments (user_id);
 CREATE INDEX user_id_created_at_idx ON posts (user_id, created_at);
+CREATE INDEX created_at_idx ON posts (created_at);
